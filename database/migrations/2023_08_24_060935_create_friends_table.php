3835018cca8589a20beaf7367cf6f610b5d74d49
alinvs94 <alin.vasiliu94@gmail.com>
2024-04-27 19:56:55 +0300

    table friends foreign id

--- database/migrations/2023_08_24_060935_create_friends_table.php
+++ database/migrations/2023_08_24_060935_create_friends_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::create('friends', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->references('id')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
-            $table->bigInteger('friend_id');
+            $table->foreignId('friend_id')->references('id')->on('users')->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
         });
     }
